26838742a39aa23783cfa2414d561a74ce69fea6
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-26 23:15:35 +0200

    Primeras configuraciones de vistas de categorias especificas

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.blog, name="Blog"),
+    path("categoria/<int:categoria_id>/", views.categoria, name="categoria"),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Categoria
+
 
 def blog(request):
 
     posts=Post.objects.all()
 
     return render(request, "blog/blog.html",{'posts':posts} )
+
+def categoria(request, categoria_id):
+
+    categoria=Categoria.objects.get(id=categoria_id)
+
+    posts=Post.objects.filter(categorias=categoria)#Para que los post que aparezcan en la vista aparezcan filtrados por categoria
+
+    return render(request, "blog/categoria.html",{'categoria':categoria,'posts':posts})
 
 # Create your views here.
