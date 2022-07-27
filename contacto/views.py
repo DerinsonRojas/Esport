3f0c39b91ab1f7509b1c7d2c1da24f2874198f8a
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-27 20:15:36 +0200

    Rescate de los valores introducidos en el diccionario y comprovación en la recarga del view contacto

--- contacto/views.py
+++ contacto/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import is_valid_path
+
 
 from .forms import FormularioContacto
 
 def contacto(request):
 
     formulario_contacto=FormularioContacto()
+
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+
+            return redirect("/contacto/?valido")
+
     return render(request, "contacto/contacto.html", {'miFormulario':formulario_contacto})
 # Create your views here.
