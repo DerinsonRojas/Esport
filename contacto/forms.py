def094d8d844667105686b137f893f750e27488c
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-27 18:48:06 +0200

    Mejoras en el formulario contactos, aplicacion de css

--- contacto/forms.py
+++ contacto/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre=forms.CharField(label='Nombre', required=True, max_length=20)
     email=forms.CharField(label='Email', required=True, max_length=30)
-    contenido=forms.CharField(label='Contenido', max_length=100)
+    contenido=forms.CharField(label='Contenido', max_length=400, widget=forms.Textarea )
