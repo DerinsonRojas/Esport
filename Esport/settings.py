f8dbcaea8799f501f1ac89cae25b4de71a94a6b6
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-28 02:28:55 +0200

    Configuración de envio de correos desde contacto

--- Esport/settings.py
+++ Esport/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from django.core.mail import EmailMessage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,12 +121,24 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+import os
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-MEDIA_URL='media/'
-MEDIA_ROOT= BASE_DIR / 'media'
+
+#MEDIA_ROOT= BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de email
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="rojasderinson@gmail.com"
+EMAIL_HOST_PASSWORD="zgjzxnaqaqrokawb"
