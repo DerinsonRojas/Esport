eed43299b6d2252cacc71e21a0037494efed841a
Derinson Rojas <derinsonrojas@gmail.com>
2022-08-03 01:36:44 +0200

    Models creados y funcionales

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CategoriaProd, Producto
+
+class CategoriaProdAdmin(admin.ModelAdmin):
+
+    readonly_fields=('created', 'updated')
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+admin.site.register(CategoriaProd, CategoriaProdAdmin)
+
+admin.site.register(Producto, ProductoAdmin)
 
 # Register your models here.
