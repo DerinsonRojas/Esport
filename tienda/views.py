33813a1c722a8926e40866e3350529679d6ef095
Derinson Rojas <derinsonrojas@gmail.com>
2022-08-03 15:20:39 +0200

    Creación de categorias y productos en el panel de administración ya está funcionales

--- tienda/views.py
+++ tienda/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def tienda(request):
 
-    return render(request, "tienda/tienda.html")
+    productos=Producto.object.all()
+
+    return render(request, "tienda/tienda.html",{'productos':productos} )
