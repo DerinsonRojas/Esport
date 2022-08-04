e4e033aad3341e1406f292f4c42fb03a166fbe95
Derinson Rojas <derinsonrojas@gmail.com>
2022-08-05 01:25:49 +0200

    Login funcional

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 
@@ -32,6 +32,24 @@ def cerrar_sesion(request):
 
     return redirect('Home')
 
+
+def logear(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get('username')
+            contra=form.cleaned_data.get('password')
+            usuario=authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.erro(request, 'Usuario no válido')
+        else:
+            messages.error('Información incorrecta')
+
+    form=AuthenticationForm()
+    return render(request, 'login/login.html', {'form':form})
 
 
 
