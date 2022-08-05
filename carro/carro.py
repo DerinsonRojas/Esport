9a2759d5a4d7f8f9502e27b7a4b88368c06436da
Derinson Rojas <derinsonrojas@gmail.com>
2022-08-05 02:14:41 +0200

    correcion del error context_processor

--- carro/carro.py
+++ carro/carro.py
@@ -1,12 +1,12 @@
 class Carro:
-    '''def __init__(self, request):
+    def __init__(self, request):
         self.request=request
         self.session=request.session
         carro=self.session.get('carro')
         if not carro:
             carro=self.session['carro']={}
         #else:
-        self.carro=carro'''
+        self.carro=carro
 
     def agregar(self, producto):
         if (str(producto.id) not in self.carro.keys()):
