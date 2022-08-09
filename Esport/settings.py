cb92505db9b4b3c78d5eab46d9746c395d472b82
Derinson Rojas <derinsonrojas@gmail.com>
2022-08-09 15:06:23 +0200

    develop funcional

--- Esport/settings.py
+++ Esport/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+8+6kwmg+ql=xljg+6eqlltawx*5t#%nb%+b(bwi-5drke!sm1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,8 +152,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER=config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER='rojasderinson@gmail.com'
+EMAIL_HOST_PASSWORD='zgjzxnaqaqrokawb'
 
 #Cargar paquete crispy
 
